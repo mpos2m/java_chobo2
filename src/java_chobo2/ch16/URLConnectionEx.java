d773e3a094c29d638fcea7530a01c28b3c2a2ea5
mpos2m <mpos2m@gmail.com>
2021-02-26 16:59:33 +0900

    ㅎ

---
+++ src/java_chobo2/ch16/URLConnectionEx.java
@@ -0,0 +1,31 @@
+package java_chobo2.ch16;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLConnectionEx {
+	public static void main(String[] args) throws MalformedURLException {
+		URL url = new URL("https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=055&aid=0000877306");
+		
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));						// 읽기위한 메서드
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("tes.html")))	// 쓰기위한 메서드
+						){
+			String line = null;
+			
+			while((line = br.readLine())!=null) {
+				System.out.println(line);
+				bw.write(line);
+			}
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
