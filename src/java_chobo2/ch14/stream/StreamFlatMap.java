c7858f8da04ac1e804913f805bc526b735f06605
mpos2m <mpos2m@gmail.com>
2021-03-05 11:27:16 +0900

    스트림중간연산 FlatMap, Optional

---
+++ src/java_chobo2/ch14/stream/StreamFlatMap.java
@@ -0,0 +1,29 @@
+package java_chobo2.ch14.stream;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StreamFlatMap {
+	public static void main(String[] args) {
+		Stream<String[]> strArrStrm = Stream.of(
+				new String[] {"abc","def","jkl"},
+				new String[] {"ABC","DEF","JKL"}
+				);
+//		Stream<Stream<String>> strStrmStrm = strArrStrm.map(Arrays::stream);
+		
+		Stream<String> strStrm = strArrStrm.flatMap(Arrays::stream);
+		strStrm.map(String::toLowerCase).distinct().sorted().forEach(System.out::println);
+		System.out.println();
+		
+		
+		String[] lineArr = {
+				"Believe or not It is true",
+				"Do or do not There is no try"
+		};
+		Stream<String> lineStream = Arrays.stream(lineArr);
+		lineStream.flatMap(line ->Stream.of(line.split(" +")))
+		.map(String::toLowerCase).distinct().sorted().forEach(System.out::println);
+		System.out.println();
+		
+	}
+}
